7feb61eabcfa4f7779ae9ba54acfc658c6f84385
Aisya08 <aisyalasum@gmail.com>
2025-09-30 22:26:17 +0700

    Add files via upload

---
+++ baru 2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// This is also a single-line comment
+
+# This is also a singe-line comment 
+
+/*
+This is a multiple-lines comment block 
+that spans over multiple 
+lines
+*/
+
+//you can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo $x;
+?>
+
+</body>
+</html>
