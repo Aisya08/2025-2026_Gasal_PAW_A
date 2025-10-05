ce8f074d9b20779cb031071d98422cf16d469d33
Aisya08 <aisyalasum@gmail.com>
2025-10-05 19:17:02 +0700

    Add files via upload

---
+++ modul2/tugas9.php
@@ -0,0 +1,22 @@
+<?php
+
+$nama = "Aisya";
+$nilai = 85;
+
+
+if ($nilai >= 85) {
+    $huruf = "A";
+} elseif ($nilai >= 70) {
+    $huruf = "B";
+} elseif ($nilai >= 55) {
+    $huruf = "C";
+} elseif ($nilai >= 40) {
+    $huruf = "D";
+} else {
+    $huruf = "E";
+}
+
+echo "Nama Mahasiswa : " . $nama . "<br>";
+echo "Nilai : " . $nilai . "<br>";
+echo "Huruf : " . $huruf;
+?>
